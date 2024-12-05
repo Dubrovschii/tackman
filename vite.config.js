9b7b1731ee366bcf67f20030677fd40ace8fcf2d
Alexei <dubrovschii@gmail.com>
2024-12-05 16:15:19 +0200

    Change vite

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,22 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// });
+// import { defineConfig } from 'vite';
+// import vue from '@vitejs/plugin-vue';
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': '/src',
     },
   },
 });
