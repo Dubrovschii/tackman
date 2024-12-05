0a758c45c3850bcc4f7b5d2f6a91e04bce976b6e
Alexei <dubrovschii@gmail.com>
2024-12-05 16:18:42 +0200

    Change settings

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,20 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, 'src'),
-//     },
-//   },
-// });
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': '/src',
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// });
